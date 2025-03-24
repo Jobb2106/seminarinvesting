1191060343f647305f5c8699803bb814b2ec4ce8
Job van Onna <638473jo@eur.nl>
2025-03-24 17:41:45 +0100

    even testen

--- RealizedVol.R
+++ RealizedVol.R
@@ -1,4 +1,23 @@
 # This script is for the programming of realized volatility
 library(ggplot2)
 
-summary(data)
+# Function
+realized_variance <- function(day_returns) {
+  sum(day_returns^2)
+}
+
+rv <- sapply(fivereturn, realized_variance)
+rv_ts <- ts(rv)
+
+
+# Plot returns
+all_returns <- unlist(fivereturn)
+time_index <- 1:length(all_returns)
+
+plot(time_index, all_returns, type = "l", col = "blue",
+     main = "5-Minute Returns Over Time",
+     xlab = "Time Index", ylab = "Return")
+
+
+
+
