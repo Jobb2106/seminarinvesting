a5501ff3e06003ecd0552915ea5bd62eff290e76
Thor Hogerbrugge <thorhogerbrugge@Thors-MacBook-Air.local>
2025-03-24 15:19:02 +0100

    snelle check

---
+++ Data.R
@@ -0,0 +1,5 @@
+# This is where the data magic happens
+
+# Import 
+
+# Subset (for coding purposes)
--- RealizedVol.R
+++ RealizedVol.R
@@ -1,7 +1,4 @@
 # This script is for the programming of realized volatility
 library(ggplot2)
-data <- data.frame()
 
 summary(data)
-hist(data)
-joehoe
