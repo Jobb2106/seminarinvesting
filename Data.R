3097b0863db3be4e409c7fe583737304ddfd43ec
Thor Hogerbrugge <thorhogerbrugge@Thors-MacBook-Air.local>
2025-03-25 17:03:18 +0100

    updates voor subsample

--- Data.R
+++ Data.R
@@ -2,10 +2,14 @@
 
 library(dplyr)
 library(readr)
+library(tidyverse)
 
 # Set folders -------------------------------------------------------------
 rds_folder <- "/Users/job/Desktop/RDS"
+# rds_folder <- "/Users/thorhogerbrugge/Desktop/RDS"
 rds_files <- list.files(rds_folder, pattern = "\\.rds$", full.names = TRUE)
+
+rds_files_test <- rds_files[6000:6005]
 
 
 # Extract date -------------------------------------------------------------------
@@ -15,19 +19,26 @@ extract_date <- function(path) {
 
 
 # Build master set ---------------------------------------
-df_all <- map_dfr(rds_files, function(file) {
+# Hier maken we 1 grote dataframe van de data zodat we mooie dingen kunnen gaan doen
+df_all <- map_dfr(rds_files_test, function(file) {
   date <- extract_date(file)
   df <- readRDS(file)
   
+  # TOCHECK
   df %>%
     filter(close_crsp > 5, close_crsp < 1000) %>%
     mutate(date = date)
 })
 
+# subset (voor het programmeren)
+saveRDS(df_all, "data/clean/returns_5m_all_subset.rds")
+
+# big file (voor de uiteindelijke resultaten)
 saveRDS(df_all, "data/clean/returns_5m_all.rds")
 
 
 # Template measures -------------------------------------------------------
+# Voorlopig niks mee doen
 df <- readRDS("data/clean/returns_5m_all.rds")
 
 df_metrics <- df %>%
