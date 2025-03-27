b8c45aa5ab871c8c8135ec0ed24b13c9b737423c
Thor Hogerbrugge <thorhogerbrugge@Thors-MacBook-Air.local>
2025-03-27 15:25:54 +0100

    realized quantiles

---
+++ Realized Quantiles.R
@@ -0,0 +1,10 @@
+# This script serves as realized quantiles code
+
+
+# Data --------------------------------------------------------------------
+# Laad de data uit GitHub
+df <- readRDS("data/clean/returns_5m_all_subset.rds")
+
+
+# Realized Quantiles ------------------------------------------------------
+
