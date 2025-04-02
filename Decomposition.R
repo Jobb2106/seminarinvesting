617a1817c50914d27704a954eb440d47aff22cbf
Thor Hogerbrugge <thorhogerbrugge@Thors-MacBook-Air.local>
2025-04-02 13:53:00 +0200

    code update

--- Decomposition.R
+++ Decomposition.R
@@ -5,12 +5,13 @@
 file_paths <- list.files("data/subset", pattern = "^filtered_\\d{4}-W\\d{2}\\.rds$", full.names = TRUE)
 
 # dropped paths
-files <- list.files("data/setdifference", pattern = "^dropped_data_\\d{4}-W\\d{2}\\.rds$", full.names = TRUE)
+dropped_files <- list.files("data/setdifference", pattern = "^dropped_data_\\d{4}-W\\d{2}\\.rds$", full.names = TRUE)
 
 # Libraries ---------------------------------------------------------------
 library(dplyr)
 library(stringr)
 library(lmtest)
+library(sandwich)
 
 
 # Negative/positive realized volatility -----------------------------------
@@ -53,84 +54,72 @@ summarise_week <- function(df, week_id) {
 # Calculations ------------------------------------------------------------
 weekly_results <- list()
 portfolio_performance <- list()
+dropped_results <- list()
 spread_data <- list()
 
 # Hierbij wil ik mijn officiele excuses aanbieden aan Jop die dit idee op het begin had. Het was een goed idee.
+# Bereken weekly_returns & RSJ voor elke week en bereken ook weekly_returns voor bedrijven die eruit vallen
 for (file in file_paths) {
   df <- readRDS(file)
   week_id <- str_remove(basename(file), "\\.rds$")
+  clean_week_id <- str_remove(week_id, "^filtered_")
   
-  # Berekeningen
+  # Berekeningen normaal
   df <- calculate_RSJ_day(df)
   df_week_summary <- summarise_week(df, week_id)
   df_week_summary$portfolio <- assign_portfolio(df_week_summary, "RSJ_week", n_portfolios = 5)
-  
   weekly_results[[week_id]] <- df_week_summary
-}
-
-# Calculate weekly returns for dropped data
-calculate_weekly_return <- function(df) {
-  df %>%
-    group_by(permno) %>%
-    summarise(
-      returns_week = mean(open_close_log_ret, na.rm = TRUE)
-    )
-}
-
-
-dropped_files <- list.files(dropped_data_path, pattern = "^dropped_data_.*\\.rds$", full.names = TRUE)
-
-for (file in dropped_files) {
-  df <- readRDS(file)
   
-  if (!"returns_week" %in% colnames(df)) {
-    weekly_df <- calculate_weekly_return(df)
+  # Berekeningen dropped
+  dropped_file <- file.path("data/setdifference", paste0("dropped_data_", clean_week_id, ".rds"))
+  if (file.exists(dropped_file)) {
+    dropped_df <- readRDS(dropped_file)
     
-    # Overwrite with the new version including weekly return
-    saveRDS(weekly_df, file)
-    message("Updated weekly return in: ", basename(file))
+    if (!"returns_week" %in% colnames(dropped_df)) {
+      dropped_df <- calculate_RSJ_day(dropped_df)
+      dropped_df <- summarise_week(dropped_df, clean_week_id)
+      dropped_results[[clean_week_id]] <- dropped_df
+    }
   }
 }
-
 
 
 # Portfolio Sorting -------------------------------------------------------
 portfolio_performance <- list()
 weekly_spreads <- list()
 
-# Voor de zekerheid
+# Sanity
 week_ids <- sort(names(weekly_results))
-dropped_data_path <- "data/setdifference/"
 
 for (i in 1:(length(week_ids) - 1)) {
-  current_week <- weekly_results[[week_ids[i]]]
-  next_week <- weekly_results[[week_ids[i + 1]]]
-  next_week_id <- sub("^filtered_", "", week_ids[i + 1])
+  current_week_id <- week_ids[i]
+  next_week_id <- week_ids[i + 1]
+  clean_next_id <- sub("^filtered_", "", next_week_id)
   
+  current_week <- weekly_results[[current_week_id]]
+  next_week <- weekly_results[[next_week_id]]
+  dropped_next <- dropped_results[[clean_next_id]]
+  
+  # Next week returns
   joined <- add_next_week_return(
     current_week_df = current_week,
     next_week_df = next_week,
-    week_id = next_week_id,
-    dropped_data_path = dropped_data_path
+    dropped_df = dropped_results[[clean_next_id]]
   )
   
-  #Optional post-processing steps:
-  # perf <- summarise_portfolios(joined)
-  # perf$week_id <- week_ids[i]
-  # portfolio_performance[[week_ids[i]]] <- perf
+  # Optional post-processing steps:
+  perf <- summarise_portfolios(joined)
+  perf$week_id <- week_ids[i]
   
-  # spread <- calculate_weekly_spreads(joined, week_ids[i])
-  # weekly_spreads[[week_ids[i]]] <- spread
+  # Store result
+  portfolio_performance[[week_ids[i]]] <- perf
 }
 
-portfolio_summary_df <- bind_rows(portfolio_performance)
 
-total_portfolio_summary <- portfolio_summary_df %>%
-  group_by(portfolio) %>%
-  summarise(
-    avg_return_overall = mean(avg_next_return, na.rm = TRUE),
-    n_weeks = n(),
-    .groups = "drop"
-  )
+# t-value for high-low spread
+performance_panel <- bind_rows(portfolio_performance)
+model <- lm(spread ~ 1, data = performance_panel)
+coeftest(model, vcov = NeweyWest)
+
 
 
