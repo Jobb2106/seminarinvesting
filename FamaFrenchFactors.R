2084c57531548ac9fa5764274ddf9e3595463f9b
Job van Onna <638473jo@eur.nl>
2025-04-03 15:34:05 +0200

    Marketcap herschreven en data import

--- FamaFrenchFactors.R
+++ FamaFrenchFactors.R
@@ -1,9 +1,14 @@
 # This script can be used to extract the necessary Fama French factors from the Kenneth French data library. This code extracts the 
 # 3 standard FF factors and also the Momentum factor. Together, this creates the Fama-French-Carhart 4-factor model. 
+
+
+# Import packages ---------------------------------------------------------
 library(tidyfinance)   # For downloading Fama-French factor data
 library(dplyr)         # For piping (%>%), mutate(), group_by(), summarize(), across()
 library(lubridate)
 
+
+# Create model ------------------------------------------------------------
 df_ff3 <- tidyfinance::download_data_factors_ff("factors_ff_3_daily", "1993-01-04", "2024-12-31") |> 
   mutate(week = floor_date(date, "weeks", week_start = "Tuesday")) |> 
   group_by(week) |>
