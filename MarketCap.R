e7beba50d7f70b82d3d653f7f0b308b0f32053de
Job van Onna <638473jo@eur.nl>
2025-04-04 10:45:05 +0200

    d

--- MarketCap.R
+++ MarketCap.R
@@ -1,6 +1,4 @@
 # This script can be used to calculate monthly market cap values, which we can use for the value-weighted portfolios and Fama-Macbeth regression 
-# NOTE: To run this script, up until now, fix the path. This can probably be improved by Job or Thor by uploading it to Git I guess? 
-# You can donwload the file from Jop's WRDS account. It has query id 9705291
 
 
 # Import packages ---------------------------------------------------------
