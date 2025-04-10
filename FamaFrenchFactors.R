f082c767f9b0c40d53615b621126b742701c830b
Thor Hogerbrugge <657476th@eur.nl>
2025-04-10 13:57:04 +0200

    Wat de kanker

--- FamaFrenchFactors.R
+++ FamaFrenchFactors.R
@@ -9,7 +9,7 @@ library(frenchdata)
 
 
 # Create model ------------------------------------------------------------
-df_ff3 <- tidyfinance::download_data_factors_ff("factors_ff_3_daily", "1993-01-04", "2024-12-31") |> 
+df_ff3 <- tidyfinance::download_data_factors_ff("factors_ff_3_daily", "1993-01-05", "2024-12-31") |> 
   mutate(week = floor_date(date, "weeks", week_start = "Tuesday")) |> 
   group_by(week) |>
   summarize(across(c("mkt_excess", "smb", "hml"), ~prod(1 + .x) - 1))
