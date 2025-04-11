a5e4878712ee7ca417e2711313b25955aabd54e3
Job van Onna <638473jo@eur.nl>
2025-04-11 11:23:54 +0200

    voorlopig af

--- CorrelationMatrix.R
+++ CorrelationMatrix.R
@@ -1,5 +1,16 @@
+# Compute correlation matrix and summary stats
 
-# Create df
+
+# Import packages ---------------------------------------------------------
+library(tidyverse)
+library(RSQLite)
+library(scales)
+library(lmtest)
+library(broom)
+library(sandwich)
+
+
+# Create df ---------------------------------------------------------------
 weekly_all_corr <- bind_rows(results) %>% 
   mutate(
     week = as.character(week),
@@ -8,6 +19,8 @@ weekly_all_corr <- bind_rows(results) %>%
   select(week, permno, RSJ_week, jr_neg, log_market_cap, next_week_return) %>%
   filter(!is.na(log_market_cap))
 
+
+# Compute correlations ----------------------------------------------------
 # Define the variables to correlate.
 vars <- c("RSJ_week", "jr_neg", "log_market_cap", "next_week_return")
 
@@ -30,3 +43,14 @@ colnames(mean_corr) <- vars
 
 # Print the final mean correlation matrix
 print(mean_corr)
+
+
+# Compute Mean and Standard Error for the Variables -----------------------
+overall_stats <- data.frame(
+  Variable = vars,
+  Mean = sapply(vars, function(v) mean(weekly_all_corr[[v]], na.rm = TRUE)),
+  StdError = sapply(vars, function(v) sd(weekly_all_corr[[v]], na.rm = TRUE) / sqrt(sum(!is.na(weekly_all_corr[[v]]))))
+)
+
+cat("\nOverall mean and standard error computed from all observations:\n")
+print(overall_stats)
